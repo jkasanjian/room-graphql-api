241e1554359059109a6a9930c67f915fb318ea61
Josh <jkasanjian@gmail.com>
2020-04-11 16:33:30 -0700

    user can update info

--- room_graphql_api/users/schema.py
+++ room_graphql_api/users/schema.py
@@ -1,7 +1,8 @@
-from .models import User
-
+from django.core.exceptions import ValidationError
 import graphene
 from graphene_django import DjangoObjectType
+
+from .models import User
 
 
 class UserType(DjangoObjectType):
@@ -30,12 +31,53 @@ class CreateUser(graphene.Mutation):
         return CreateUser(user=user)
 
 
+class UserInput(graphene.InputObjectType):
+        username    = graphene.String()
+        password    = graphene.String()
+        first_name  = graphene.String()
+        last_name   = graphene.String()
+        status      = graphene.String()
+
+
+class UpdateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        user_data = UserInput(required=True)
+
+    def mutate(self, info, user_data=None):
+        user = info.context.user
+
+        for k, v in user_data.items():
+            if(k == 'password') and (v is not None):
+                user.set_password(user_data.password)
+            else:
+                setattr(user, k, v)
+        
+        try:
+            user.full_clean()
+            user.save()
+            return UpdateUser(user=user)
+        
+        except ValidationError as e:
+            return UpdateUser(user=user, errors=e)
+
+
 class Query(graphene.ObjectType):
     users = graphene.List(UserType)
+    me = graphene.Field(UserType)
 
     def resolve_users(self, info):
         return User.objects.all()
 
+    def resolve_me(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('Not logged in')
+
+        return user
+
 
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
+    update_user = UpdateUser.Field()
